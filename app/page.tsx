f4cc8abe0088d91b5e7e1c73ea94c3459729f9bd
TugberkAta <atatugberk@yahoo.com>
2024-03-12 19:26:18 +0200

    Add new preview component
      Add some padding between containers

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Banner from "@/common/components/homepage/banner";
+import Banner from "@/common/components/homepage/Banner";
+import Previews from "@/common/components/homepage/Previews";
 
 export default function Home() {
   return (
     <>
-      <Banner></Banner>
+      <div className="w-screen">
+        <Banner></Banner>
+        <div className=" ml-16 mr-16 mt-6 mb-6">
+          <Previews></Previews>
+        </div>
+      </div>
     </>
   );
 }
