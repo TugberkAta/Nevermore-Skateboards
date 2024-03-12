39a12cec02d57cb415e512afe7e8b6441d59d5f4
TugberkAta <atatugberk@yahoo.com>
2024-03-12 19:24:33 +0200

    Add new fonts

--- common/styles/fonts.ts
+++ common/styles/fonts.ts
@@ -1,4 +1,10 @@
-import { Inter, Nunito, Inika, Inknut_Antiqua } from "next/font/google";
+import {
+  Inter,
+  Nunito,
+  Inika,
+  Inknut_Antiqua,
+  Montserrat,
+} from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
@@ -7,3 +13,5 @@ export const nunito = Nunito({ subsets: ["latin"] });
 export const inika = Inika({ subsets: ["latin"], weight: "700" });
 
 export const inknut = Inknut_Antiqua({ subsets: ["latin"], weight: "700" });
+
+export const montserrat = Montserrat({ subsets: ["latin"], weight: "700" });
