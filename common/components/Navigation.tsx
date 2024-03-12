f938252e0185965cbbeab570a92f4fbf2911356b
TugberkAta <atatugberk@yahoo.com>
2024-03-12 22:07:41 +0200

    Update the anchors of products to go to their own roots

--- common/components/Navigation.tsx
+++ common/components/Navigation.tsx
@@ -8,15 +8,15 @@ export default function Navigation() {
     <nav className={`w-screen h-14 flex items-center`}>
       <div className="ml-8 mr-8 flex items-center justify-between w-screen">
         <div className="flex items-center">
-          <div className="flex gap-4 h-full items-center ">
+          <a href="/" className="flex gap-4 h-full items-center ">
             <Image width={34} height={34} src="/raven.svg" alt="Raven Icon" />
             <p className={`${inika.className}`}>Nevermore</p>
-          </div>
+          </a>
           <div className="ml-20 font-semibold gap-5 text-sm flex text-black">
-            <a href="">Skateboards</a>
-            <a href="">Roller Blades</a>
-            <a href="">Snowboards</a>
-            <a href="">Shoes</a>
+            <a href="/products/Skateboards">Skateboards</a>
+            <a href="/products/RollerBlades">Roller Blades</a>
+            <a href="/products/Snowboards">Snowboards</a>
+            <a href="/products/Shoes">Shoes</a>
           </div>
         </div>
         <div className="fill-black flex gap-4">
