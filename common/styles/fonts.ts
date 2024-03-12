2739ed8e6578c3f1ad8af01321aa471c255252d9
TugberkAta <atatugberk@yahoo.com>
2024-03-12 16:20:26 +0200

    Initial commit

---
+++ common/styles/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Nunito, Inika, Inknut_Antiqua } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const nunito = Nunito({ subsets: ["latin"] });
+
+export const inika = Inika({ subsets: ["latin"], weight: "700" });
+
+export const inknut = Inknut_Antiqua({ subsets: ["latin"], weight: "700" });
