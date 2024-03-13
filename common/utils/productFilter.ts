aa24f7e8b216605b5325bd11983b1d0142caae0d
TugberkAta <atatugberk@yahoo.com>
2024-03-13 11:50:05 +0200

    Add new file to put select options into

---
+++ common/utils/productFilter.ts
@@ -0,0 +1,23 @@
+const Options = {
+  GeneralOptions: [
+    { value: "default", label: "Default" },
+    { value: "highToLow", label: "High To Low" },
+    { value: "lowToHigh", label: "Low To High" },
+  ],
+  SkateSizeOptions: [
+    { value: "default", label: "Default" },
+    { value: "8", label: "8" },
+    { value: "7,75", label: "7,75" },
+  ],
+  PriceRangeOptions: [
+    { value: "default", label: "Default" },
+    { value: "0", label: "0" },
+    { value: "10", label: "10" },
+    { value: "20", label: "20" },
+    { value: "30", label: "30" },
+    { value: "50", label: "50" },
+    { value: "100", label: "100" },
+  ],
+};
+
+export default Options;
