5a0b81ec59ec2ec3073bd521b747fb288c32663d
TugberkAta <atatugberk@yahoo.com>
2024-03-13 11:52:14 +0200

    Add sliding animation to nav tabs
    Add a new homepage nav
    
    Turn navs into an array that gets mapped into views

--- common/components/Navigation.tsx
+++ common/components/Navigation.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import { inika } from "../styles/fonts";
 import { IoMdSearch } from "react-icons/io";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { useParams } from "next/navigation";
+
+// Array of every tab to be shown in the nav bar
+const tabs = [
+  { id: undefined, text: "Homepage" },
+  { id: "Skateboards", text: "Skateboards" },
+  { id: "RollerBlades", text: "Roller Blades" },
+  { id: "Snowboards", text: "Snowboards" },
+  { id: "Shoes", text: "Shoes" },
+];
 
 export default function Navigation() {
+  // Get params for conditionally updating active tab
+  const params = useParams<{ id: any }>();
+
+  // State for updating the active tab
+  const [activeTab, setActiveTab] = useState(params.id);
+
   return (
     <nav className={`w-screen h-14 flex items-center`}>
       <div className="ml-8 mr-8 flex items-center justify-between w-screen">
@@ -12,11 +32,28 @@ export default function Navigation() {
             <Image width={34} height={34} src="/raven.svg" alt="Raven Icon" />
             <p className={`${inika.className}`}>Nevermore</p>
           </a>
-          <div className="ml-20 font-semibold gap-5 text-sm flex text-black">
-            <a href="/products/Skateboards">Skateboards</a>
-            <a href="/products/RollerBlades">Roller Blades</a>
-            <a href="/products/Snowboards">Snowboards</a>
-            <a href="/products/Shoes">Shoes</a>
+          <div
+            className="ml-20 font-semibold gap-5 text-sm flex text-black"
+            onMouseLeave={() => setActiveTab(params.id)}
+          >
+            {tabs.map((tab) => (
+              <a
+                key={tab.id}
+                href={tab.id != undefined ? "/products/" + tab.id : `/`}
+                onMouseEnter={() => setActiveTab(tab.id)}
+                className="relative"
+              >
+                {tab.text}
+                {activeTab === tab.id && (
+                  /*This component uses layoutId to animate between positions*/
+                  <motion.div
+                    layoutId="underline"
+                    className="absolute bg-black w-full h-[0.10rem]"
+                    transition={{ duration: 0.7, type: "spring" }}
+                  ></motion.div>
+                )}
+              </a>
+            ))}
           </div>
         </div>
         <div className="fill-black flex gap-4">
