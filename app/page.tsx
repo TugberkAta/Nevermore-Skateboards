bd28f74fe0029d68ca3fff0b3bf93b40d53ff2f7
TugberkAta <atatugberk@yahoo.com>
2024-03-14 14:14:37 +0200

    Move nav into the components as some didnt need it

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/common/components/homepage/Banner";
 import Previews from "@/common/components/homepage/Previews";
+import Navigation from "@/common/components/Navigation";
 
 export default function Home() {
   return (
     <>
+      <Navigation></Navigation>
       <div className="w-screen">
         <Banner></Banner>
         <div className=" ml-16 mr-16 mt-6 mb-6">
