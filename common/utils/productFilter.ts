452ddb7f3fa214082ecc4abea2a2a6cde595dee9
TugberkAta <atatugberk@yahoo.com>
2024-03-17 21:06:19 +0200

    Change the price range values into integer

--- common/utils/productFilter.ts
+++ common/utils/productFilter.ts
@@ -11,12 +11,12 @@ const Options = {
   ],
   PriceRangeOptions: [
     { value: "default", label: "Default" },
-    { value: "0", label: "0" },
-    { value: "10", label: "10" },
-    { value: "20", label: "20" },
-    { value: "30", label: "30" },
-    { value: "50", label: "50" },
-    { value: "100", label: "100" },
+    { value: 0, label: 0 },
+    { value: 10, label: 10 },
+    { value: 20, label: 20 },
+    { value: 30, label: 30 },
+    { value: 50, label: 50 },
+    { value: 100, label: 100 },
   ],
 };
 
