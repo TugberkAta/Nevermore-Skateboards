b67971149d9d2d7bbb68c900ce96b080d06a9c07
TugberkAta <atatugberk@yahoo.com>
2024-03-19 16:05:03 +0200

    Replace the old option object into an interactive option list that fetches data and puts options depending on that

--- common/utils/productFilter.ts
+++ common/utils/productFilter.ts
@@ -1,23 +1,72 @@
-const Options = {
-  GeneralOptions: [
-    { value: "default", label: "Default" },
-    { value: "highToLow", label: "High To Low" },
-    { value: "lowToHigh", label: "Low To High" },
-  ],
-  SkateSizeOptions: [
-    { value: "default", label: "Default" },
-    { value: "8", label: "8" },
-    { value: "7,75", label: "7,75" },
-  ],
-  PriceRangeOptions: [
-    { value: "default", label: "Default" },
-    { value: 0, label: 0 },
-    { value: 10, label: 10 },
-    { value: 20, label: 20 },
-    { value: 30, label: 30 },
-    { value: 50, label: 50 },
-    { value: 100, label: 100 },
-  ],
-};
+"use server";
 
-export default Options;
+interface Option {
+  value: string | number;
+  label: string;
+}
+
+export interface Options {
+  GeneralOptions: Option[];
+  ShoeSizeOptions: Option[];
+  RollerbladeSizeOptions: Option[];
+  SkateSizeOptions: Option[];
+  SnowboardSizeOptions: Option[];
+  PriceRangeOptions: Option[];
+}
+
+import {
+  fetchRollerbladeData,
+  fetchShoeData,
+  fetchSkateData,
+  fetchSnowboardData,
+} from "../lib/data";
+
+export async function createOptions() {
+  const skateData = await fetchSkateData();
+  const rollerbladeData = await fetchRollerbladeData();
+  const snowboardData = await fetchSnowboardData();
+  const shoesData = await fetchShoeData();
+
+  const Options: Options = {
+    GeneralOptions: [
+      { value: "default", label: "Default" },
+      { value: "highToLow", label: "High To Low" },
+      { value: "lowToHigh", label: "Low To High" },
+    ],
+    ShoeSizeOptions: [
+      { value: "default", label: "Default" },
+      ...shoesData.sizeData.map((e) => {
+        return { value: e.size, label: e.size };
+      }),
+    ],
+    RollerbladeSizeOptions: [
+      { value: "default", label: "Default" },
+      ...rollerbladeData.sizeData.map((e) => {
+        return { value: e.size, label: e.size };
+      }),
+    ],
+    SkateSizeOptions: [
+      { value: "default", label: "Default" },
+      ...skateData.sizeData.map((e) => {
+        return { value: e.size, label: e.size };
+      }),
+    ],
+    SnowboardSizeOptions: [
+      { value: "default", label: "Default" },
+      ...snowboardData.sizeData.map((e) => {
+        return { value: e.size, label: e.size };
+      }),
+    ],
+    PriceRangeOptions: [
+      { value: "default", label: "Default" },
+      { value: 0, label: "0" },
+      { value: 20, label: "20" },
+      { value: 50, label: "50" },
+      { value: 100, label: "100" },
+      { value: 200, label: "200" },
+      { value: 500, label: "500" },
+    ],
+  };
+
+  return Options;
+}
