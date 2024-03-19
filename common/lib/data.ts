1fb5bc249715b55becef1c06e8af8ba620773c94
TugberkAta <atatugberk@yahoo.com>
2024-03-19 11:23:48 +0200

    Add a new controller to fetch an item by its uuid

--- common/lib/data.ts
+++ common/lib/data.ts
@@ -1,13 +1,13 @@
 import { sql } from "@vercel/postgres";
 import { unstable_noStore as noStore } from "next/cache";
 
-type Item = {
-  uuid?: number;
-  title?: string;
-  price?: number;
-  brand?: string;
-  size?: number;
-  img_url?: string;
+export type Item = {
+  uuid: string;
+  title: string;
+  price: number;
+  brand: string;
+  size: number;
+  img_url: string;
 };
 
 export async function fetchSkateData() {
@@ -59,6 +59,26 @@ export async function fetchSnowboardData() {
   try {
     const data = await sql<Item>`SELECT * FROM snowboards`;
 
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch snowboard data.");
+  }
+}
+
+export async function fetchItemData(uuid: string) {
+  // Add noStore() here to prevent the response from being cached.
+  noStore();
+
+  try {
+    const data = await sql<Item>`SELECT * FROM snowboards WHERE uuid = ${uuid}
+    UNION
+    SELECT * FROM skates WHERE uuid = ${uuid}
+    UNION
+    SELECT * FROM rollerblades WHERE uuid = ${uuid}
+    UNION
+    SELECT * FROM shoes WHERE uuid = ${uuid}
+    `;
     return data.rows;
   } catch (error) {
     console.error("Database Error:", error);
