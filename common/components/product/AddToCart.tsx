3f5a82bf012127dddb537165b37dd9814b174e17
TugberkAta <atatugberk@yahoo.com>
2024-03-20 19:31:20 +0200

    Add a index finder that checks if an item has already been added to the card and if so increases their count

--- common/components/product/AddToCart.tsx
+++ common/components/product/AddToCart.tsx
@@ -4,16 +4,52 @@ import { useEffect, useState } from "react";
 import { DetailsProps } from "./ProductDetails";
 
 export function AddToCart({ itemData }: DetailsProps) {
-  const [shopCart, setShopCart] = useState<string[]>([]);
+  const [shopCart, setShopCart] = useState<string[]>(() => {
+    if (typeof window !== "undefined") {
+      const storedCart = localStorage.getItem("shopCart");
+      return storedCart ? JSON.parse(storedCart) : [];
+    } else null;
+  });
 
   useEffect(() => {
     // Store the shopCart array as a string in localStorage
     localStorage.setItem("shopCart", JSON.stringify(shopCart));
+    window.dispatchEvent(new Event("storage"));
   }, [shopCart]);
 
   const handleAddToCart = () => {
-    // Use the spread operator to include existing items, and add the new uuid
-    setShopCart((prevShopCart) => [...prevShopCart, itemData.uuid]);
+    const newItem = {
+      uuid: itemData.uuid,
+      img_url: itemData.img_url,
+      price: itemData.price,
+      size: itemData.size,
+      title: itemData.title,
+      brand: itemData.brand,
+      count: 1,
+    };
+
+    const itemIndex = shopCart.findIndex((itemString) => {
+      const item = JSON.parse(itemString);
+      return item.uuid === newItem.uuid;
+    });
+
+    if (itemIndex !== -1) {
+      // If the item exists, update its count
+      setShopCart((prevShopCart) =>
+        prevShopCart.map((itemString, index) => {
+          if (index === itemIndex) {
+            const item = JSON.parse(itemString);
+            item.count += 1;
+            return JSON.stringify(item);
+          }
+          return itemString;
+        })
+      );
+    } else {
+      // If the item doesn't exist, add it to the cart
+      const newItemString = JSON.stringify(newItem);
+      setShopCart((prevShopCart) => [...prevShopCart, newItemString]);
+    }
   };
 
   return (
