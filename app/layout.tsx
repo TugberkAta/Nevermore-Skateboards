05e59cc666e9f784e93c3e8cf5f9fc18215f059f
TugberkAta <atatugberk@yahoo.com>
2024-03-25 09:38:43 +0200

    Add SpeedInsight and Analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "../common/styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +18,8 @@ export default function RootLayout({
   return (
     <html className="h-full" lang="en">
       <body className={`${inter.className} h-full`}>{children}</body>
+      <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
