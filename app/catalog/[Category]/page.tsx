ec1403d5d6f892c76159ea3f64701b5ec322b942
TugberkAta <atatugberk@yahoo.com>
2024-03-25 09:29:32 +0200

    Add suspense to async components

--- app/catalog/[Category]/page.tsx
+++ app/catalog/[Category]/page.tsx
@@ -1,5 +1,7 @@
 import FetchFilterOptions from "@/common/components/catalog/FetchFilterOptions";
 import FetchProductData from "@/common/components/catalog/FetchProductData";
+import FilterSkeleton from "@/common/components/skeletons/filterSkeleton";
+import { Suspense } from "react";
 
 export default async function ProductPage({
   params,
@@ -8,8 +10,12 @@ export default async function ProductPage({
 }) {
   return (
     <>
-      <FetchFilterOptions></FetchFilterOptions>
-      <FetchProductData productName={params.Category} />
+      <Suspense fallback={<FilterSkeleton></FilterSkeleton>}>
+        <FetchFilterOptions productName={params.Category}></FetchFilterOptions>
+      </Suspense>
+      <Suspense fallback={<div></div>}>
+        <FetchProductData productName={params.Category} />
+      </Suspense>
     </>
   );
 }
