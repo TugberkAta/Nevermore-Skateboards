1d57c366d364a7532d7090bf8651f0f024003e8f
TugberkAta <atatugberk@yahoo.com>
2024-03-25 14:22:57 +0200

    Add new skeleton ui for products

--- app/catalog/[Category]/page.tsx
+++ app/catalog/[Category]/page.tsx
@@ -1,6 +1,7 @@
 import FetchFilterOptions from "@/common/components/catalog/FetchFilterOptions";
 import FetchProductData from "@/common/components/catalog/FetchProductData";
 import FilterSkeleton from "@/common/components/skeletons/filterSkeleton";
+import ProductSkeleton from "@/common/components/skeletons/productSkeleton";
 import { Suspense } from "react";
 
 export default async function ProductPage({
@@ -13,7 +14,7 @@ export default async function ProductPage({
       <Suspense fallback={<FilterSkeleton></FilterSkeleton>}>
         <FetchFilterOptions productName={params.Category}></FetchFilterOptions>
       </Suspense>
-      <Suspense fallback={<div></div>}>
+      <Suspense fallback={<ProductSkeleton></ProductSkeleton>}>
         <FetchProductData productName={params.Category} />
       </Suspense>
     </>
