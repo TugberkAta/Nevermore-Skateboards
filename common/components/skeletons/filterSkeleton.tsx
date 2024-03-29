0d4a621b169bfd3475f72cf78741385854a67a1d
TugberkAta <atatugberk@yahoo.com>
2024-03-29 20:11:48 +0200

    Update ui of the filter skeleton

--- common/components/skeletons/filterSkeleton.tsx
+++ common/components/skeletons/filterSkeleton.tsx
@@ -2,34 +2,50 @@ import { FaArrowsUpDown } from "react-icons/fa6";
 
 export default function FilterSkeleton() {
   return (
-    <div className="flex h-5/6">
-      <div className="flex flex-col gap-3 ml-4 w-fit p-4">
-        <div>
-          <p className="font-bold mb-3 text-3xl">FILTER</p>
-        </div>
-        <div className="text-xl font-bold">Sort By</div>
-        <div className="w-40 h-8 animate-pulse relative">
-          <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
-        </div>
-        <div className="text-xl font-bold">Size</div>
-        <div className="w-40 h-8 animate-pulse relative">
-          <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
-        </div>
-        <div className="text-xl font-bold">Price Range</div>
-        <div className="flex flex-col items-center gap-1">
-          <div className="w-40 h-8 animate-pulse relative">
-            <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+    <div className="h-5/6 w-60 hidden lg:block">
+      <div className=" gap-4 pr-10 pl-10 items-center ">
+        <p className="font-bold text-3xl mb-8">FILTER</p>
+        <div className="flex w-full flex-col flex-wrap  md:flex-nowrap mb-6 md:mb-0 gap-4">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <div className="h-8 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
+            <div className="relative">
+              <div className="h-12 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
           </div>
-          Min
-          <FaArrowsUpDown />
-          Max
-          <div className="w-40 h-8 animate-pulse relative">
-            <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <div className="h-8 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
+            <div className="relative">
+              <div className="h-12 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
           </div>
-        </div>
-        <div className="text-xl font-bold">Brand</div>
-        <div className="w-40 h-8 animate-pulse relative">
-          <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <div className="h-8 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
+            <div className="relative">
+              <div className="h-12 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <div className="h-8 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
+            <div className="relative">
+              <div className="h-12 w-48"></div>
+              <div className="w-full h-full bg-black opacity-10 transition-opacity top-0 absolute z-20"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
