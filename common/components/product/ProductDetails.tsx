d75d7751f449d0c4c5055deaf047fdf611b4046d
TugberkAta <atatugberk@yahoo.com>
2024-03-30 21:02:16 +0200

    Update padding after adding breadcrumbs to the ui

--- common/components/product/ProductDetails.tsx
+++ common/components/product/ProductDetails.tsx
@@ -14,18 +14,18 @@ export interface DetailsProps {
 export default function ProductDetails({ itemData, sizeData }: DetailsProps) {
   return (
     <>
-      <div className="flex w-screen flex-col md:flex-row gap-20 lg:pb-0 pb-40 mt-20">
+      <div className="mt-10 flex w-screen flex-col gap-20 pb-40 md:flex-row lg:pb-0">
         <div className="w-full md:w-1/2">
           <ProductImage
             alt={itemData.title || ""}
             src={itemData.img_url || ""}
           ></ProductImage>
         </div>
-        <div className="w-full md:w-1/2  flex justify-center">
+        <div className="flex w-full justify-center md:w-1/2">
           <div className="w-10/12">
             <ProductInformation itemData={itemData} sizeData={sizeData} />
             <AddToCart itemData={itemData}></AddToCart>
-            <div className="flex flex-col gap-4 text-sm text-gray-500 mt-8">
+            <div className="mt-8 flex flex-col gap-4 text-sm text-gray-500">
               <div className="flex items-center gap-6">
                 <HiOutlineTruck className="size-6 stroke-gray-500" />
                 <p>Free Expedited Shipping</p>
@@ -45,34 +45,34 @@ export default function ProductDetails({ itemData, sizeData }: DetailsProps) {
 export function ProductInformation({ itemData, sizeData }: DetailsProps) {
   return (
     <div>
-      <div className="pb-4 mb-4">
+      <div className="mb-4 pb-4">
         <h3 className={`${montserratThin.className} text-sm`}>
           {itemData.brand}
         </h3>
         <h1 className={`${montserrat.className} text-2xl`}>{itemData.title}</h1>
-        <div className={`${montserratThin.className} text-gray-500 mt-3 mb-3`}>
+        <div className={`${montserratThin.className} mb-3 mt-3 text-gray-500`}>
           £{(itemData.price || 0) / 100} STR
         </div>
-        <p className="text-gray-700 text-sm">
+        <p className="text-sm text-gray-700">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor illum
           aspernatur reprehenderit cumque at neque deserunt, eveniet sapiente
           consequuntur nam rerum recusandae magni tenetur eius, ad quis deleniti
           natus harum!
         </p>
       </div>
-      <div className="w-full flex justify-center">
-        <div className="flex items-center justify-center w-10/12 flex-col">
-          <p className={` ${openSans.className} text-xs font-bold mb-3`}>
+      <div className="flex w-full justify-center">
+        <div className="flex w-10/12 flex-col items-center justify-center">
+          <p className={` ${openSans.className} mb-3 text-xs font-bold`}>
             SIZE
           </p>
           <div
-            className={`${montserrat.className} text-sm flex flex-wrap justify-center w-full gap-3 mb-2`}
+            className={`${montserrat.className} mb-2 flex w-full flex-wrap justify-center gap-3 text-sm`}
           >
             {sizeData?.map((type) => {
               return (
                 <div key={type.size}>
                   <button
-                    className={`border relative w-12 h-8 rounded-3xl text-center ${
+                    className={`relative h-8 w-12 rounded-3xl border text-center ${
                       type.size === itemData.size
                         ? "border-black bg-black text-white"
                         : ""
@@ -82,7 +82,7 @@ export function ProductInformation({ itemData, sizeData }: DetailsProps) {
                       <p>{type.size}</p>
                     ) : (
                       <>
-                        <p className="text-gray-500 cursor-not-allowed">
+                        <p className="cursor-not-allowed text-gray-500">
                           {type.size}
                         </p>
                       </>
