75910424eec9abf50033ea27e099619ff9c24e1d
TugberkAta <atatugberk@yahoo.com>
2024-03-30 20:05:15 +0200

    Add new component for credit

---
+++ common/components/footer/Credit.tsx
@@ -0,0 +1,7 @@
+export default function Credit() {
+  return (
+    <div className="w-full flex justify-center lg:justify-end lg:pr-10 text-center text-sm pb-4 pt-6 text-gray-400">
+      © 2024 Made by Tuğberk Ata
+    </div>
+  );
+}
