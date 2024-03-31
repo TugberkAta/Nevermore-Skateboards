8ecce41b88dfda6fbb1b34c90bebd5fc42f1495c
TugberkAta <atatugberk@yahoo.com>
2024-03-31 19:29:47 +0300

    Update the component so that it now uses an array to showcase multiple pictures
    Create a state to track the highlighted picture
    
    Add an animation that shows the current tracked picture
    
    Change the ui to suit the new added pictures

--- common/components/product/ProductImage.tsx
+++ common/components/product/ProductImage.tsx
@@ -1,23 +1,53 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
 
 type ProductImageProps = {
   alt: string;
-  src: string;
+  img_url_arr: string[];
 };
 
-export function ProductImage({ alt, src }: ProductImageProps) {
+export function ProductImage({ alt, img_url_arr }: ProductImageProps) {
+  // activeImg state that is tracked based on the picture the user clicks
+  const [activeImg, setActiveImg] = useState<string>(img_url_arr[0]);
+
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative col-span-1 w-full grid-rows-1 ">
-        <div className="group flex h-full w-full items-center justify-center ">
-          <Image
-            width={700}
-            height={700}
-            className={`h-auto w-fit scale-90 transition-transform duration-300 ease-in-out group-hover:scale-100`}
-            src={src}
-            alt={alt}
-          />
-        </div>
+    <div className="grid h-full min-h-80 w-full grid-cols-4 grid-rows-1 ">
+      <div className="col-span-1 grid grid-cols-1 grid-rows-3">
+        {img_url_arr.map((img_url, index) => {
+          return (
+            <div
+              key={"image-preview-" + alt + "-" + index}
+              className={`relative cursor-pointer`}
+              onClick={() => setActiveImg(img_url)}
+            >
+              <Image
+                fill
+                className={`h-auto w-full scale-90 object-contain transition-transform duration-300 ease-in-out group-hover:scale-100`}
+                src={img_url}
+                alt={"image-preview-" + alt + "-" + index}
+              ></Image>
+              {activeImg === img_url && (
+                /*This component uses layoutId to animate between positions*/
+                <motion.div
+                  layoutId="border-right"
+                  className="absolute right-0 h-full w-[0.10rem] bg-gray-300"
+                  transition={{ duration: 0.7, type: "spring" }}
+                ></motion.div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="group relative col-span-3">
+        <Image
+          fill
+          className={`h-auto w-full scale-90 object-contain transition-transform duration-300 ease-in-out group-hover:scale-100`}
+          src={activeImg}
+          alt={alt}
+        ></Image>
       </div>
     </div>
   );
