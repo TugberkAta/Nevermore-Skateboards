95a9d9df13be4b1fccbdd0c428f0c09528ef59f4
TugberkAta <atatugberk@yahoo.com>
2024-04-02 20:50:13 +0300

    Update the input fields for brand and size to be selectors with predetermined values
    Add two more imageUrl fields for the preview images
    
    Add brandData and sizeData  values for the options of selectors
    
    Add a new state to track the multi select input field

--- common/components/CRUD/CrudCreate.tsx
+++ common/components/CRUD/CrudCreate.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { Providers } from "@/app/providers";
 import { createRollerblades } from "@/common/lib/actions/rollerbladeAction";
 import { createShoes } from "@/common/lib/actions/shoesAction";
 import { createSkate } from "@/common/lib/actions/skateActions";
 import { createSnowboards } from "@/common/lib/actions/snowboardAction";
+import {
+  CategoryBrandData,
+  CategorySizeData,
+} from "@/common/utils/categorySpecificData";
+import { Select, SelectItem } from "@nextui-org/select";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 
 type CrudCreateProps = {
@@ -25,7 +32,7 @@ export default function CrudCreate({ itemCategory }: CrudCreateProps) {
   let create;
 
   switch (itemCategory) {
-    case "Skates":
+    case "Skateboards":
       create = createSkate;
       break;
     case "Rollerblades":
@@ -41,12 +48,55 @@ export default function CrudCreate({ itemCategory }: CrudCreateProps) {
       create = () => Promise.reject("Invalid item category");
   }
 
+  let sizeData;
+  switch (itemCategory) {
+    case "Skateboards":
+      sizeData = CategorySizeData.Skateboards;
+      break;
+    case "Rollerblades":
+      sizeData = CategorySizeData.Rollerblade;
+      break;
+    case "Snowboards":
+      sizeData = CategorySizeData.Snowboards;
+      break;
+    case "Shoes":
+      sizeData = CategorySizeData.Shoes;
+      break;
+    default:
+      return;
+  }
+
+  let brandData;
+  switch (itemCategory) {
+    case "Skateboards":
+      brandData = CategoryBrandData.Skateboards;
+      break;
+    case "Rollerblades":
+      brandData = CategoryBrandData.Rollerblade;
+      break;
+    case "Snowboards":
+      brandData = CategoryBrandData.Snowboards;
+      break;
+    case "Shoes":
+      brandData = CategoryBrandData.Shoes;
+      break;
+    default:
+      return;
+  }
+
   const [state, formAction] = useFormState(create, initialState);
+
+  const [sizeValues, setSizeValues] = useState(new Set([]));
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <>
       <div className="flex w-screen justify-center">
         <div className="mb-16 mt-16 flex w-fit flex-col items-center justify-center gap-6 rounded-md rounded-t-none border-t-4 border-black bg-slate-100  px-24 py-6 shadow-md">
-          <p className="text-xl font-bold"> {itemCategory} Creation</p>
+          <p className="text-xl font-bold">Creating {itemCategory}</p>
           <form
             className="flex flex-col items-center gap-4"
             action={formAction}
@@ -63,23 +113,37 @@ export default function CrudCreate({ itemCategory }: CrudCreateProps) {
               placeholder={"Enter $ Amount"}
               label="USD Amount"
             ></DefaultInput>
-            <DefaultInput
+            <SelectInput
               name={"brand"}
-              type={"input"}
-              placeholder={"Enter Brand Name"}
-              label="Brand Name"
-            ></DefaultInput>
-            <DefaultInput
+              data={brandData}
+              placeholder={"Pick A Brand"}
+              label={"Brand"}
+            ></SelectInput>
+            <SelectMultipleInput
               name={"size"}
+              data={sizeData}
+              placeholder={"Pick Available Sizes"}
+              label={"Size"}
+              setValue={setSizeValues}
+              values={sizeValues}
+            ></SelectMultipleInput>
+            <DefaultInput
+              name={"imgUrl"}
               type={"input"}
-              placeholder={"Enter Size"}
-              label="Size"
+              placeholder={"Enter Main Image Url"}
+              label="Main Image Url"
             ></DefaultInput>
             <DefaultInput
               name={"imgUrl"}
               type={"input"}
-              placeholder={"Enter Image Url"}
-              label="Image Url"
+              placeholder={"Enter Image #2"}
+              label="Image Url #2"
+            ></DefaultInput>
+            <DefaultInput
+              name={"imgUrl"}
+              type={"input"}
+              placeholder={"Enter Image #3"}
+              label="Image Url #3"
             ></DefaultInput>
             <button
               className="rounded bg-black px-4 py-2 font-bold text-white hover:bg-gray-800"
@@ -107,5 +171,80 @@ function DefaultInput({ name, type, placeholder, label }: DefaultInputProps) {
       <label htmlFor={name}>{label}</label>
       <input type={type} id={name} name={name} placeholder={placeholder} />
     </div>
+  );
+}
+
+type SelectMultipleInputProps = {
+  name: string;
+  data: string[];
+  placeholder: string;
+  label: string;
+  setValue: Dispatch<SetStateAction<Set<never>>>;
+  values: Set<never>;
+};
+
+function SelectMultipleInput({
+  name,
+  placeholder,
+  label,
+  data,
+  setValue,
+  values,
+}: SelectMultipleInputProps) {
+  const handleSelectionChange = (e: any) => {
+    setValue(new Set(e.target.value.split(",")));
+  };
+  return (
+    <Providers>
+      <label htmlFor={name}>{label}</label>
+      <Select
+        aria-label={label}
+        id={name}
+        name={name}
+        selectionMode="multiple"
+        placeholder={placeholder}
+        variant="bordered"
+        className="w-60"
+        isRequired
+        selectedKeys={values}
+        onChange={handleSelectionChange}
+      >
+        {data.map((option) => (
+          <SelectItem className="bg-gray-50" key={option} value={option}>
+            {option}
+          </SelectItem>
+        ))}
+      </Select>
+    </Providers>
+  );
+}
+
+type SelectInputProps = {
+  name: string;
+  data: string[];
+  placeholder: string;
+  label: string;
+};
+
+function SelectInput({ name, placeholder, label, data }: SelectInputProps) {
+  return (
+    <Providers>
+      <label htmlFor={name}>{label}</label>
+      <Select
+        aria-label={label}
+        id={name}
+        name={name}
+        variant="bordered"
+        placeholder={placeholder}
+        className="w-60"
+        isRequired
+      >
+        {data.map((option) => (
+          <SelectItem className="bg-gray-50" key={option} value={option}>
+            {option}
+          </SelectItem>
+        ))}
+      </Select>
+    </Providers>
   );
 }
