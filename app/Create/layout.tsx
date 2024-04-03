304e27a21a33e23a194df3b6fce6b018f5a87dd9
TugberkAta <atatugberk@yahoo.com>
2024-04-03 21:25:58 +0300

    Delete the needles div that was causing bugs for the layout

--- app/Create/layout.tsx
+++ app/Create/layout.tsx
@@ -5,9 +5,5 @@ export default function ProductsLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <section className="h-full w-screen bg-gray-700">
-      <div>{children}</div>
-    </section>
-  );
+  return <section className="h-full w-screen bg-gray-700">{children}</section>;
 }
