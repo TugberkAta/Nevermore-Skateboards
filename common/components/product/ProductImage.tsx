704febea4d90f2518c4c17dcc3d0432919088139
TugberkAta <atatugberk@yahoo.com>
2024-04-05 16:36:02 +0300

    Update the component to add a new magnifier states and functions to zoom in the active image

--- common/components/product/ProductImage.tsx
+++ common/components/product/ProductImage.tsx
@@ -2,16 +2,58 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 
 type ProductImageProps = {
   alt: string;
   img_url_arr: string[];
 };
 
+// Constants for the magnifier
+const MAGNIFIER_SIZE = 200;
+const ZOOM_LEVEL = 1.5;
+
 export function ProductImage({ alt, img_url_arr }: ProductImageProps) {
   // activeImg state that is tracked based on the picture the user clicks
   const [activeImg, setActiveImg] = useState<string>(img_url_arr[0]);
+
+  // State variables for magnifier
+  const [zoomable, setZoomable] = useState(false);
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
+  // Saves the positions of image and the mouse
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+    mouseX: 0,
+    mouseY: 0,
+  });
+
+  // Event handlers for magnifier
+  const handleMouseEnter = (e: MouseEvent) => {
+    let element = e.currentTarget;
+    let { width, height } = element.getBoundingClientRect();
+    setImageSize({ width, height });
+    setZoomable(true);
+    updatePosition(e);
+  };
+  const handleMouseLeave = (e: MouseEvent) => {
+    setZoomable(false);
+    updatePosition(e);
+  };
+  const handleMouseMove = (e: MouseEvent) => {
+    updatePosition(e);
+  };
+  const updatePosition = (e: MouseEvent) => {
+    const { left, top } = e.currentTarget.getBoundingClientRect();
+    let x = e.clientX - left;
+    let y = e.clientY - top;
+    setPosition({
+      x: -x * ZOOM_LEVEL + MAGNIFIER_SIZE / 2,
+      y: -y * ZOOM_LEVEL + MAGNIFIER_SIZE / 2,
+      mouseX: x - MAGNIFIER_SIZE / 2,
+      mouseY: y - MAGNIFIER_SIZE / 2,
+    });
+  };
 
   return (
     <div className="grid h-full min-h-80 w-full grid-cols-4 grid-rows-1 ">
@@ -41,13 +83,34 @@ export function ProductImage({ alt, img_url_arr }: ProductImageProps) {
           );
         })}
       </div>
-      <div className="group relative col-span-3">
+      <div
+        className="group relative col-span-3 cursor-none"
+        onMouseLeave={handleMouseLeave}
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouseMove}
+      >
+        {/* Active Image */}
         <Image
           fill
-          className={`h-auto w-full scale-90 object-contain transition-transform duration-300 ease-in-out group-hover:scale-100`}
+          className={`h-auto w-full object-contain`}
           src={activeImg}
           alt={alt}
         ></Image>
+        {/* Magnifier */}
+        <div
+          style={{
+            backgroundPosition: `${position.x}px ${position.y}px`,
+            backgroundImage: `url(${activeImg})`,
+            backgroundSize: `${imageSize.width * ZOOM_LEVEL}px ${imageSize.height * ZOOM_LEVEL}px`,
+            backgroundRepeat: "no-repeat",
+            display: zoomable ? "block" : "none",
+            top: `${position.mouseY}px`,
+            left: `${position.mouseX}px`,
+            width: `${MAGNIFIER_SIZE}px`,
+            height: `${MAGNIFIER_SIZE}px`,
+          }}
+          className={`pointer-events-none absolute z-50 rounded-full border-1`}
+        />
       </div>
     </div>
   );
