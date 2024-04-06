e1e4a9cac0c70d9b87559554905e1f158948161a
TugberkAta <atatugberk@yahoo.com>
2024-04-06 20:50:04 +0300

    Update the logic of component from rendering image bases on index to rendering them initially and changing the opacity to use transition to better ui

--- common/components/catalog/PreviewItem.tsx
+++ common/components/catalog/PreviewItem.tsx
@@ -2,24 +2,58 @@ import { montserrat, montserratMedium } from "@/common/styles/fonts";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 type PreviewItemProps = {
   alt: string;
-  src: string;
+  srcArray: string[];
   title: string;
   price: number;
   address: string;
   brand: string;
 };
 
+const DurationOfAnimation = 1500;
+
 export function PreviewItem({
   alt,
-  src,
+  srcArray,
   title,
   price,
   address,
   brand,
 }: PreviewItemProps) {
+  const [activeImageIndex, setActiveImageIndex] = useState<number>(0);
+  const [activeAnimation, setActiveAnimation] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [intervalId]);
+
+  function handleMouseEnter() {
+    setActiveAnimation(true);
+    const id = setInterval(() => {
+      setActiveImageIndex((currentIndex) =>
+        currentIndex < srcArray.length - 1 ? currentIndex + 1 : 0,
+      );
+    }, DurationOfAnimation); // Change image every 2 seconds
+    setIntervalId(id);
+  }
+
+  function handleMouseLeave() {
+    setActiveAnimation(false);
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    setActiveImageIndex(0); // Reset image to the first one when mouse leaves
+    setIntervalId(null);
+  }
+
   return (
     <motion.div
       layout
@@ -31,16 +65,46 @@ export function PreviewItem({
         href={address}
         className="flex h-full w-full flex-col items-center justify-center overflow-hidden pb-2 "
       >
-        <div className="relative h-[16.5rem] w-full overflow-hidden md:h-[18.5rem] ">
+        <div
+          className="relative h-[16.5rem] w-full overflow-hidden md:h-[18.5rem] "
+          onMouseOver={handleMouseEnter}
+          onMouseOut={handleMouseLeave}
+        >
           <div className="group flex h-full w-full justify-center ">
             <div className="h-5/6">
-              <Image
-                loading="lazy"
-                fill
-                className={`h-auto w-full scale-90 object-contain transition-transform duration-300 ease-in-out group-hover:scale-100`}
-                src={src}
-                alt={alt}
-              />
+              {srcArray.map((src) => {
+                return (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={
+                      src === srcArray[activeImageIndex]
+                        ? { opacity: 100 }
+                        : { opacity: 0 }
+                    }
+                    transition={{ duration: 0.3 }}
+                  >
+                    <Image
+                      loading="lazy"
+                      fill
+                      className={`h-auto w-full object-contain`}
+                      src={src}
+                      alt={alt}
+                    />
+                  </motion.div>
+                );
+              })}
+              <motion.div
+                initial={{ width: 0 }}
+                animate={{ width: activeAnimation ? "100%" : "0%" }}
+                transition={{
+                  duration: activeAnimation
+                    ? (DurationOfAnimation / 1000) * srcArray.length
+                    : 0,
+                  repeat: Infinity,
+                  ease: "linear",
+                }}
+                className="absolute bottom-0 left-0 h-[0.1rem] w-full bg-gray-700 opacity-60"
+              ></motion.div>
             </div>
             <div className="absolute top-0 z-20 h-full w-full bg-orange-950 opacity-5 transition-opacity"></div>
           </div>
