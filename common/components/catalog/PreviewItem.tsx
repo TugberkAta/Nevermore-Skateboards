d4ff34ea4157cab216d470a1a6fec7001ef82aca
TugberkAta <atatugberk@yahoo.com>
2024-04-11 18:36:46 +0300

    Update the component to make it also be suitable for using in the carousel

--- common/components/catalog/PreviewItem.tsx
+++ common/components/catalog/PreviewItem.tsx
@@ -11,6 +11,8 @@ type PreviewItemProps = {
   price: number;
   address: string;
   brand: string;
+  disableSlide?: boolean;
+  children?: React.ReactNode;
 };
 
 const DurationOfAnimation = 1500;
@@ -22,6 +24,8 @@ export function PreviewItem({
   price,
   address,
   brand,
+  disableSlide = false,
+  children,
 }: PreviewItemProps) {
   const [activeImageIndex, setActiveImageIndex] = useState<number>(0);
   const [activeAnimation, setActiveAnimation] = useState(false);
@@ -66,45 +70,57 @@ export function PreviewItem({
         className="flex h-full w-full flex-col items-center justify-center overflow-hidden pb-2 "
       >
         <div
-          className="relative h-[16.5rem] w-full overflow-hidden md:h-[18.5rem] "
+          className="relative h-[16.5rem] w-full overflow-hidden rounded-2xl shadow-sm md:h-[18.5rem] "
           onMouseOver={handleMouseEnter}
           onMouseOut={handleMouseLeave}
         >
           <div className="group flex h-full w-full justify-center ">
             <div className="h-5/6">
-              {srcArray.map((src) => {
-                return (
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={
-                      src === srcArray[activeImageIndex]
-                        ? { opacity: 100 }
-                        : { opacity: 0 }
-                    }
-                    transition={{ duration: 0.3 }}
-                  >
-                    <Image
-                      loading="lazy"
-                      fill
-                      className={`h-auto w-full object-contain`}
-                      src={src}
-                      alt={alt}
-                    />
-                  </motion.div>
-                );
-              })}
-              <motion.div
-                initial={{ width: 0 }}
-                animate={{ width: activeAnimation ? "100%" : "0%" }}
-                transition={{
-                  duration: activeAnimation
-                    ? (DurationOfAnimation / 1000) * srcArray.length
-                    : 0,
-                  repeat: Infinity,
-                  ease: "linear",
-                }}
-                className="absolute bottom-0 left-0 h-[0.1rem] w-full bg-gray-700 opacity-60"
-              ></motion.div>
+              {disableSlide ? (
+                <Image
+                  loading="lazy"
+                  fill
+                  className={`h-auto w-full scale-90 object-contain`}
+                  src={srcArray[0]}
+                  alt={alt}
+                />
+              ) : (
+                srcArray.map((src) => {
+                  return (
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      animate={
+                        src === srcArray[activeImageIndex]
+                          ? { opacity: 100 }
+                          : { opacity: 0 }
+                      }
+                      transition={{ duration: 0.3 }}
+                    >
+                      <Image
+                        loading="lazy"
+                        fill
+                        className={`h-auto w-full scale-90 object-contain`}
+                        src={src}
+                        alt={alt}
+                      />
+                    </motion.div>
+                  );
+                })
+              )}
+              {!disableSlide && (
+                <motion.div
+                  initial={{ width: 0 }}
+                  animate={{ width: activeAnimation ? "100%" : "0%" }}
+                  transition={{
+                    duration: activeAnimation
+                      ? (DurationOfAnimation / 1000) * srcArray.length
+                      : 0,
+                    repeat: Infinity,
+                    ease: "linear",
+                  }}
+                  className="absolute bottom-0 left-0 h-[0.1rem] w-full bg-gray-700 opacity-60"
+                ></motion.div>
+              )}
             </div>
             <div className="absolute top-0 z-20 h-full w-full bg-orange-950 opacity-5 transition-opacity"></div>
           </div>
@@ -120,6 +136,7 @@ export function PreviewItem({
           </div>
           <p className="text-xs">£{price / 100}</p>
         </div>
+        {children}
       </Link>
     </motion.div>
   );
