f6315b806cce4f03c98c0c16c4d3fee52892b7fa
TugberkAta <atatugberk@yahoo.com>
2024-04-11 18:37:28 +0300

    Remove repeated code and use previewitem component instead to show items

--- common/components/homepage/Carousel.tsx
+++ common/components/homepage/Carousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { Suspense, useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { Item, ItemsWithCategory } from "@/common/lib/data";
 import Autoplay from "embla-carousel-autoplay";
@@ -8,6 +8,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { montserratMedium } from "@/common/styles/fonts";
 import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import { PreviewItem } from "../catalog/PreviewItem";
 
 export function Carousel({
   latestItems,
@@ -32,43 +33,33 @@ export function Carousel({
         LATEST ARRIVALS
       </p>
       <div className="w-4/6">
-        <div className=" overflow-hidden" ref={emblaRef}>
+        <div className="overflow-hidden" ref={emblaRef}>
           <div className="flex">
             {latestItems.map((item) => {
               return (
-                <Link
+                <div
                   key={item.title + "carousel"}
-                  className="relative flex flex-col items-center justify-center p-4 md:flex-[0_0_55%] lg:flex-[0_0_45%] xl:flex-[0_0_40%] 2xl:flex-[0_0_30%]"
-                  href={`/product/${item.category}/${item.uuid}`}
+                  className="relative p-4 md:flex-[0_0_55%] lg:flex-[0_0_45%] xl:flex-[0_0_40%] 2xl:flex-[0_0_30%]"
                 >
-                  <div className="relative min-h-80 w-full">
-                    <p className="absolute left-2 top-2 z-20 rounded-sm bg-black px-2 font-mono text-white">
+                  <PreviewItem
+                    alt={item.title}
+                    srcArray={item.img_url_arr}
+                    title={item.title}
+                    price={item.price}
+                    address={`/product/${item.category}/${item.uuid}`}
+                    brand={item.brand}
+                    disableSlide={true}
+                  >
+                    <p className="absolute bottom-0 right-4 z-20 rounded-sm bg-black px-2 font-mono text-white">
                       New Arrival
                     </p>
-                    <Image
-                      className="className=w-full scale-90 object-contain"
-                      fill
-                      src={item.img_url}
-                      alt={item.title}
-                    ></Image>
-                    <div className="absolute top-0 z-20 h-full w-full bg-orange-950 opacity-5 transition-opacity"></div>
-                  </div>
-                  <div className="flex w-full justify-between pb-4 pt-2">
-                    <div className="flex flex-col ">
-                      <p className="text-base font-semibold">{item.brand}</p>
-                      <p
-                        className={`h-4 overflow-hidden font-sans text-xs font-semibold`}
-                      >
-                        {item.title}
-                      </p>
-                    </div>
-                    <p className="text-xs">£{item.price / 100}</p>
-                  </div>
-                </Link>
+                  </PreviewItem>
+                </div>
               );
             })}
           </div>
-          <div className="flex gap-4">
+
+          <div className="mt-8 flex gap-4">
             <button onClick={scrollPrev}>
               <IoIosArrowDropleft className="size-6"></IoIosArrowDropleft>
             </button>
