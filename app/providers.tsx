9f363b97a17ee7a6dd6963ea9615fbd16ac87de8
TugberkAta <atatugberk@yahoo.com>
2024-04-12 22:34:09 +0300

    Move navigation inside page component
    Remove layout as its not being used anymore

--- app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { NextUIProvider } from "@nextui-org/react";
-
-// Wrap to use NextUi components
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
