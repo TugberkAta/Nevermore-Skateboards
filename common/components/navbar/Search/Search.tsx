143490dd05db4b0f0708263be9d51082d5564638
TugberkAta <atatugberk@yahoo.com>
2024-04-14 22:07:39 +0300

    Add responsive design for smaller screens
    Pass props for mobile search section
    
    Add a overlay when search is active
    
    Fix ui issue with the search that was caused by padding, taking it down to the child fixed it

--- common/components/navbar/Search/Search.tsx
+++ common/components/navbar/Search/Search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, SetStateAction, Dispatch } from "react";
 import { Input } from "@nextui-org/react";
 import { Providers } from "@/common/components/providers";
 import { IoSearch } from "react-icons/io5";
@@ -12,9 +12,15 @@ import { motion } from "framer-motion";
 
 type SearchProps = {
   queryItems: ItemsWithCategory[];
+  activeSearchBar: boolean;
+  setActiveSearchBar: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function Search({ queryItems }: SearchProps) {
+export default function Search({
+  queryItems,
+  setActiveSearchBar,
+  activeSearchBar,
+}: SearchProps) {
   const [focused, setFocused] = useState(false);
 
   /**
@@ -27,6 +33,7 @@ export default function Search({ queryItems }: SearchProps) {
        */
       function handleClickOutside(event: { target: any }) {
         if (ref.current && !ref.current.contains(event.target)) {
+          setActiveSearchBar(false);
           setFocused(false);
         }
       }
@@ -72,40 +79,72 @@ export default function Search({ queryItems }: SearchProps) {
     replace(`${pathname}?${params.toString()}`);
   }, [queryParams]);
 
+  // To disable scrolling when the search-bar is focused and opened on mobile
+  useEffect(() => {
+    if (activeSearchBar) {
+      document.getElementById("query")?.focus();
+    }
+    if (focused) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [focused, activeSearchBar]);
+
   return (
-    <div className="relative" onFocus={() => setFocused(true)} ref={wrapperRef}>
-      <Providers>
-        <Input
-          id="query"
-          className="w-[22rem]"
-          placeholder="Type to search..."
-          defaultValue={searchParams.get("query")?.toString()}
-          startContent={<IoSearch className="size-5 fill-gray-500"></IoSearch>}
-          onChange={(e) => handleQueryChange("query", e?.target.value || "")}
-        ></Input>
-      </Providers>
-      {queryItems && focused && (
-        <motion.div
-          className="flex h-[90%] flex-col items-center justify-between"
-          layout
+    <>
+      <div
+        className={`${focused ? "absolute left-0 top-0 z-30 flex h-screen w-screen bg-black opacity-30" : "hidden"}`}
+      ></div>
+      <div
+        className={`${activeSearchBar ? "absolute left-0 top-20 z-40 flex w-full justify-center" : "hidden"} ${focused ? "z-40" : ""} lg:block`}
+      >
+        <div
+          className="relative"
+          onFocus={() => setFocused(true)}
+          ref={wrapperRef}
         >
-          <div className="absolute top-11 z-30 flex w-full flex-col items-center gap-4 overflow-scroll rounded-lg bg-white py-7">
-            {queryItems?.map((item) => {
-              return (
-                <PreviewSearchItems
-                  alt={item.title || ""}
-                  src={item.img_url}
-                  title={item.title || ""}
-                  price={item.price || NaN}
-                  address={`/product/${item.category}/${item.uuid}`}
-                  brand={item.brand}
-                  uuid={item.uuid}
-                ></PreviewSearchItems>
-              );
-            })}
-          </div>
-        </motion.div>
-      )}
-    </div>
+          <Providers>
+            <Input
+              id="query"
+              className="w-[80vw] lg:w-[22rem]"
+              placeholder="Type to search..."
+              defaultValue={searchParams.get("query")?.toString()}
+              startContent={
+                <IoSearch className="size-5 fill-gray-500"></IoSearch>
+              }
+              onChange={(e) =>
+                handleQueryChange("query", e?.target.value || "")
+              }
+            ></Input>
+          </Providers>
+          {queryItems && focused && (
+            <motion.div
+              className="flex h-[90%] flex-col items-center justify-between"
+              layout
+            >
+              <div
+                className={`absolute top-11 z-30 flex w-full flex-col items-center gap-4 overflow-scroll rounded-lg bg-white [&>div:first-child]:pt-4 [&>div:last-child]:pb-4`}
+              >
+                {queryItems?.map((item) => {
+                  return (
+                    <PreviewSearchItems
+                      key={item.title + " search result"}
+                      alt={item.title || ""}
+                      src={item.img_url}
+                      title={item.title || ""}
+                      price={item.price || NaN}
+                      address={`/product/${item.category}/${item.uuid}`}
+                      brand={item.brand}
+                      uuid={item.uuid}
+                    ></PreviewSearchItems>
+                  );
+                })}
+              </div>
+            </motion.div>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
