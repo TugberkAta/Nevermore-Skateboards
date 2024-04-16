343016bc1173a5fa9296128a272b2fa83c76d918
TugberkAta <atatugberk@yahoo.com>
2024-04-16 13:21:33 +0300

    Update the skeleton to better suit the current version of catalog page

--- common/components/skeletons/productSkeleton.tsx
+++ common/components/skeletons/productSkeleton.tsx
@@ -6,11 +6,11 @@ export default function ProductSkeleton() {
     arrayOfSkeletonBox.push(
       <div
         key={index}
-        className="flex animate-pulse flex-col items-center overflow-hidden "
+        className="flex animate-pulse flex-col overflow-hidden rounded-md "
       >
         <div className="relative col-span-1 mb-1 w-full grid-rows-1 overflow-hidden">
           <div className="group flex h-full w-full justify-center">
-            <div className="h-[250px] w-[350px]" />
+            <div className="h-[16.5rem] w-[24.5rem]" />
             <div className="absolute top-0 z-20 h-full w-full bg-black opacity-10 transition-opacity"></div>
           </div>
         </div>
@@ -24,7 +24,7 @@ export default function ProductSkeleton() {
     );
   }
   return (
-    <div className="ml-10 mr-10 grid h-full auto-rows-auto grid-cols-2 gap-x-8 gap-y-4 pt-10 md:grid-cols-3 lg:grid-cols-3">
+    <div className=" grid h-full grid-cols-2 gap-x-8 gap-y-4 md:grid-cols-2  lg:grid-cols-3 2xl:grid-cols-5">
       {arrayOfSkeletonBox}
     </div>
   );
